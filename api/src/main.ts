cc09c2e98a515f48f73e4fbb7c14ae9b829c07b2
Justice Roberts <justice.roberts@fortyau.com>
2023-04-21 16:25:51 -0500

    fix: Add SwaggerUI to backend

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger UI setup
+  const config = new DocumentBuilder()
+    .setTitle('Quackabot API')
+    .setDescription('API to manage member data for Qackabot')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config); 
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
